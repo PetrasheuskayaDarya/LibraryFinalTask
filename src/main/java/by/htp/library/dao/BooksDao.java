2a044e18539a4094f3b2a18f2c1adf1182388358
Darya <darya.petrasheuskaya@gmail.com>
2018-08-02 13:23:07 +0300

    The sceleton for project is created

---
+++ src/main/java/by/htp/library/dao/BooksDao.java
@@ -0,0 +1,14 @@
+package by.htp.library.dao;
+
+import java.awt.print.Book;
+import java.util.List;
+
+
+public interface BooksDao {
+	Book read(int id);
+	List<Book> list();
+	void add(Book book);
+	void delete(Book book);
+	void update(Book book);
+
+}
---
+++ src/main/java/by/htp/library/dao/imple/BooksDaoImpl.java
@@ -0,0 +1,5 @@
+package by.htp.library.dao.imple;
+
+public class BooksDaoImpl {
+
+}
