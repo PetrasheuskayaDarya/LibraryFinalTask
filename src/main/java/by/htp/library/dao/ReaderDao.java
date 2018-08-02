1595675a70bc1dd40306f8886b777317222f68bc
Darya <darya.petrasheuskaya@gmail.com>
2018-08-02 13:26:58 +0300

    Class ReaderDao is created

--- src/main/java/by/htp/library/dao/ReaderDao.java
+++ src/main/java/by/htp/library/dao/ReaderDao.java
@@ -1,5 +1,15 @@
 package by.htp.library.dao;
 
+
+import java.util.List;
+
+import by.htp.library.entity.Reader;
+
 public interface ReaderDao {
+	Reader read(int id);
+	List<Reader> list();
+	void add(Reader reader);
+	void delete(Reader reader);
+	void update(Reader reader);
 
 }
